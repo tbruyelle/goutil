8309d346d7496503ae9499b9200f08d153cc9b98
Thomas Bruyelle <thomas.bruyelle@gmail.com>
2014-06-04 11:30:02 +0200

    New method IsFile()

--- goutil.go
+++ goutil.go
@@ -4,10 +4,26 @@ import (
 	"os"
 )
 
+// IsFile returns true if the file exists and is not a directory.
+func IsFile(path string) bool {
+	exists, fi := fileExists(path)
+	if exists && !fi.IsDir() {
+		return true
+	}
+	return false
+}
+
 // FileExists returns true if the file exists.
 func FileExists(path string) bool {
-	if _, err := os.Stat(path); os.IsNotExist(err) {
-		return false
+	exists,_ := fileExists(path)
+	return exists
+}
+
+func fileExists(path string) (bool, os.FileInfo) {
+	fi, err := os.Stat(path)
+	os.IsNotExist(err)
+	if err != nil && os.IsNotExist(err) {
+		return false, nil
 	}
-	return true
+	return true, fi
 }
